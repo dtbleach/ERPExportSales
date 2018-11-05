d6af95559ea40ecea8a8164f5c3e9ffcce5a57d2
leon <334395083@qq.com>
2018-11-05 16:41:40 +0800

    add project

---
+++ ERP.ExportSales.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ERP.ExportSales.Web.MvcApplication" Language="C#" %>
---
+++ ERP.ExportSales.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ERP.ExportSales.Web.Startup))]
+namespace ERP.ExportSales.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
