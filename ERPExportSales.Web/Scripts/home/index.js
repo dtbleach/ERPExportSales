16637afe25a5e103786e917ed98a5ef87c9abccd
leon <334395083@qq.com>
2018-12-13 16:46:04 +0800

    22

--- ERPExportSales.Web/Scripts/home/index.js
+++ ERPExportSales.Web/Scripts/home/index.js
@@ -17,41 +17,74 @@ function initIndex(usertype) {
         "pageLength": 15,
         "lengthChange": false
     });
-            
+    $('#infoUl a:last').click(function (e) {
+        e.preventDefault();
+        $(this).tab('show');
+        $("#usdcnyDiv").css("width", $("#usdcnyDiv").width());
+        $("#usdcnyDiv").css("height", "300px");
+        $("#q195Div").css("width", $("#q195Div").width());
+        $("#q195Div").css("height", "300px");
 
-    $.ajax({
-        type: "POST",
-        url: "/Chart/GetUSDCNY",
-        dataType: "json",
-        success: function (data) {
-            setTimeout(drawUSDCNYLines(data), 3000);
+        var q195div = echarts.getInstanceByDom(document.getElementById("q195Div"));
+        var usdcnyDiv = echarts.getInstanceByDom(document.getElementById("usdcnyDiv"));
+        if (q195div != null && q195div != "" && q195div != undefined) {
+            q195div.dispose();
         }
-    });
-    $.ajax({
-        type: "POST",
-        url: "/Chart/GetQ195",
-        dataType: "json",
-        success: function (data) {
-            drawQ195Lines(data);
+        if (usdcnyDiv != null && usdcnyDiv != "" && usdcnyDiv != undefined) {
+            usdcnyDiv.dispose();
         }
-    });
+        var myChart1 = echarts.init(document.getElementById('usdcnyDiv'));
+        var myChart2 = echarts.init(document.getElementById('q195Div'));
+        myChart1.showLoading();
+        myChart2.showLoading();
+        $.ajax({
+            type: "POST",
+            url: "/Chart/GetUSDCNY",
+            dataType: "json",
+            success: function (data) {
+                drawUSDCNYLines(myChart1, data);
+                myChart1.hideLoading();
+                //  myChart1.resize();
+            }
+        });
+        $.ajax({
+            type: "POST",
+            url: "/Chart/GetQ195",
+            dataType: "json",
+            success: function (data) {
+                drawQ195Lines(myChart2, data);
+                myChart2.hideLoading();
+                //  myChart2.resize();
+            }
+        });
 
-    $.ajax({
-        type: "POST",
-        url: "/Chart/GetNewUSDCNY",
-        dataType: "json",
-        success: function (data) {
-            $("#newUSDCNY").html("Update time:  <b>" + data.PublishDate + "</b> USDCNY: <h4 class='box-title'><b>" + data.Price + "</b></h4>");
+        $.ajax({
+            type: "POST",
+            url: "/Chart/GetNewUSDCNY",
+            dataType: "json",
+            success: function (data) {
+                $("#newUSDCNY").html("Update time:  <b>" + data.PublishDate + "</b> USDCNY: <h4 class='box-title'><b>" + data.Price + "</b></h4>");
+            }
+        });
+        $.ajax({
+            type: "POST",
+            url: "/Chart/GetNewQ195",
+            dataType: "json",
+            success: function (data) {
+                $("#newQ195").html("Update time:  <b>" + data.PublishDate + "</b> Price: <h4 class='box-title'><b>" + data.Price + "</b></h4> RMB/T");
+            }
+        });
+         q195div = echarts.getInstanceByDom(document.getElementById("q195Div"));
+         usdcnyDiv = echarts.getInstanceByDom(document.getElementById("usdcnyDiv"));
+        window.onresize = function () {
+            $("#usdcnyDiv").css("width", "100%");
+            $("#q195Div").css("width", "100%");
+            q195div.resize();
+            usdcnyDiv.resize();
         }
-    });
-    $.ajax({
-        type: "POST",
-        url: "/Chart/GetNewQ195",
-        dataType: "json",
-        success: function (data) {
-            $("#newQ195").html("Update time:  <b>" + data.PublishDate + "</b> Price: <h4 class='box-title'><b>" + data.Price + "</b></h4> RMB/T");
-        }
-    });
+     
+    })
+
     //$.getJSON("../../Files/data/data.json", function (data, status) {
     //    if (status == 'success') {
     //        result = data;
@@ -64,19 +97,18 @@ function initIndex(usertype) {
 
 
     // 根据配置项和数据显示图表
-         
-    var q195div = echarts.getInstanceByDom(document.getElementById("q195Div"));
-    var usdcnyDiv = echarts.getInstanceByDom(document.getElementById("usdcnyDiv"));
-    window.addEventListener("resize", function () {
+    hrefChangePasswordPage(usertype);
+}
+
+function tabEventListener(q195div,usdcnyDiv) {
         if (q195div != null && q195div != undefined) {
+            $("#q195Div").css("width", $("#q195Div").width());
             q195div.resize();
         }
         if (usdcnyDiv != null && usdcnyDiv != undefined) {
+            $("#usdcnyDiv").css("width", $("#usdcnyDiv").width());
             usdcnyDiv.resize();
         }
-    });
-
-    hrefChangePasswordPage(usertype);
 }
 
 function hrefChangePasswordPage(usertype) {
@@ -94,11 +126,7 @@ function hrefChangePasswordPage(usertype) {
 }
 
 
-function drawUSDCNYLines(data) {
-    $("#usdcnyDiv").css("width", "720px");
-    $("#usdcnyDiv").css("height", "300px");
-    var myChart = echarts.init(document.getElementById('usdcnyDiv'));
-    myChart.showLoading();
+function drawUSDCNYLines(myChart, data) {
     myChart.setOption({
         title: {
             text: 'USDCNY',
@@ -153,12 +181,8 @@ function drawUSDCNYLines(data) {
             //}
         ]
     });
-    myChart.hideLoading();
 }
-function drawQ195Lines(data) {
-    $("#q195Div").css("width", "720px");
-    $("#q195Div").css("height", "300px");
-    var myChart = echarts.init(document.getElementById('q195Div'));
+function drawQ195Lines(myChart, data) {
     myChart.setOption({
         title: {
             text: 'Q195',
